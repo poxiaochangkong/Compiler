23b555780cac5cf5a0b41345605db1c5a66af9d8
四方 <2273403820@qq.com>
2025-08-08 22:39:50 +0800

    标准输入

--- src/main.cpp
+++ src/main.cpp
@@ -150,12 +150,13 @@ int main(int argc, char** argv) {
         else {
             // 如果命令行中除了 "-opt" (或什么都没有) 外，没有提供输入文件
             if (argc > 1) { // 如果提供了参数但没找到文件名
-                std::cerr << "Error: No input file specified." << std::endl;
+                /*std::cerr << "Error: No input file specified." << std::endl;
                 std::cerr << "Usage: Compiler [-opt] <input_file>" << std::endl;
                 for (int i = 0; i < argc; ++i) {
                     std::cerr << "argv[" << i << "]: " << argv[i] << std::endl;
                 }
                 return 1;
+                平台输入来自标准输入*/
             }
             // 如果未提供任何参数 (argc <= 1)，程序将默认从标准输入读取，行为和原来一致
         }
